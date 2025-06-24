23f21f8238f6b569a7d561a284b9ba4b9031cb5d
praesidi <edik.halilov1999@gmail.com>
2025-06-24 10:10:55 +0300

    feat: add sorting on category page

--- src/pages/category/category.tsx
+++ src/pages/category/category.tsx
@@ -1,21 +1,64 @@
 import styles from "./category.module.css";
+import type { Location } from "../../entities/location";
+import type { Character } from "../../entities/character";
+import type { Episode } from "../../entities/episode";
+import type { SingleValue } from "react-select";
+import { useCallback, useEffect, useState } from "react";
+import { formatDate } from "../../shared/utils/date";
 import { Link, useParams } from "react-router";
 import { Grid, Item } from "../../widgets/grid/grid";
 import { useFakeFetch } from "../../shared/hooks/useFakeFetch";
 import { Loader } from "../../shared/components/loader/loader";
-import { useCallback } from "react";
-import type { Location } from "../../entities/location";
-import type { Character } from "../../entities/character";
-import type { Episode } from "../../entities/episode";
 import { internalPaths } from "../../shared/constants/routes";
+import { Select, type Option } from "../../shared/components/select/select";
 
 type Entity = Character | Location | Episode;
 
 const entities = ["characters", "locations", "episodes"];
+const sortingOptions: Option[] = [
+  { value: "", label: "none" },
+  { value: "create_asc", label: "create date ASC" },
+  { value: "create_desc", label: "create date DESC" },
+];
 
 export const Category = () => {
   const { category } = useParams();
   const { data, isLoading } = useFakeFetch<Entity>(category);
+  const [sortedData, setSortedData] = useState<Entity[] | null>(null);
+  const [selectedOption, setSelectedOption] =
+    useState<SingleValue<Option>>(null);
+
+  useEffect(() => {
+    let sorted = data;
+
+    if (data) {
+      switch (selectedOption?.value) {
+        case "create_asc":
+          sorted = data.sort(
+            (cur, next) =>
+              new Date(next.created).getTime() - new Date(cur.created).getTime()
+          );
+          break;
+
+        case "create_desc":
+          sorted = data.sort(
+            (cur, next) =>
+              new Date(cur.created).getTime() - new Date(next.created).getTime()
+          );
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    setSortedData(sorted);
+  }, [data, selectedOption?.value]);
+
+  const handleFilterUpdate = useCallback((newVal: SingleValue<Option>) => {
+    console.log(newVal);
+    setSelectedOption(newVal);
+  }, []);
 
   const getContent = useCallback(() => {
     if (isLoading) {
@@ -26,7 +69,7 @@ export const Category = () => {
       );
     }
 
-    if (data === null || data.length === 0) {
+    if (sortedData === null || sortedData.length === 0) {
       return <p>Data Not Found</p>;
     }
 
@@ -35,27 +78,42 @@ export const Category = () => {
     }
 
     return (
-      <Grid>
-        {data.map((item) => {
+      <Grid className={styles.content}>
+        {sortedData?.map((item) => {
           return (
             <Link
               key={item.id}
               to={internalPaths.item(category, String(item.id))}
             >
               <Item>
-                <h3>{item.name}</h3>
-                <p>{item.id}</p>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <div style={{ display: "flex", justifyContent: "center" }}>
+                    <h3>{item.name}</h3>
+                    <p className={styles.item_id}>[{item.id}]</p>
+                  </div>
+                  <p className={styles.item_id}>
+                    created: [{formatDate(item.created, "dd.mm.yyyy")}]
+                  </p>
+                </div>
               </Item>
             </Link>
           );
         })}
       </Grid>
     );
-  }, [category, data, isLoading]);
+  }, [category, sortedData, isLoading]);
 
   return (
     <>
       <h1 className={styles.title}>{category}</h1>
+      <div className={styles.select_wrapper}>
+        <Select
+          value={selectedOption}
+          defaultValue={null}
+          options={sortingOptions}
+          onChange={handleFilterUpdate}
+        />
+      </div>
       <>{getContent()}</>
     </>
   );
