afa380fd39c5e106e40796c8ab1aa15503ad855f
praesidi <edik.halilov1999@gmail.com>
2025-07-06 14:58:12 +0300

    refac: refactor routing

--- src/shared/constants/routes.ts
+++ src/shared/constants/routes.ts
@@ -4,10 +4,14 @@ interface AppRoutes {
   home: string;
   category: (name: Category) => string;
   item: (name: Category, id: string) => string;
+  login: string;
+  not_found: string;
 }
 
 export const internalPaths: AppRoutes = {
   home: "/",
   category: (name: string): string => `/${name}`,
   item: (name: string, id: string): string => `/${name}/${id}`,
+  login: "/sign_in",
+  not_found: "*",
 };
